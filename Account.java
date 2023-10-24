071eee476afcfa539ba9fb357933573c3dfa282f
Tobias <tobiasnf@gmail.com>
2023-10-24 16:49:20 +0200

    the balance cannot go below 0

--- Account.java
+++ Account.java
@@ -1,18 +1,24 @@
 public class Account {
-    private int balance;
-    Account(int startBalance) {
-        this.balance = startBalance;
-    }
 
-    public int getBalance() {
-        return balance;
-    }
+  private int balance;
 
-    public void setBalance(int balance) {
-        this.balance = balance;
+  Account(int startBalance) {
+    this.balance = startBalance;
+  }
+
+  public int getBalance() {
+    return balance;
+  }
+
+  public void setBalance(int balance) {
+    if (balance < 0) {
+      this.balance = 0;
+    } else {
+      this.balance = balance;
     }
-    
-    public String toString() {
-        return "The account has a balance of: " + this.balance;
-    }
+  }
+
+  public String toString() {
+    return "The account has a balance of: " + this.balance;
+  }
 }
