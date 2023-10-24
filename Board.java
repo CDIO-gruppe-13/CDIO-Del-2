8cfe6d2c3577fa6d5f54b2841a7f2215f9e3d7f1
Tobias <tobiasnf@gmail.com>
2023-10-24 14:37:15 +0200

    added getters to Board class

--- Board.java
+++ Board.java
@@ -1,12 +1,24 @@
 public class Board {
-    private String name;
-    private int points;
-    private String description;
 
-    public Board(String name, int points, String description) {
-        this.name = name;
-        this.points = points;
-        this.description = description;
-    }
+  private String name;
+  private int points;
+  private String description;
 
+  public Board(String name, int points, String description) {
+    this.name = name;
+    this.points = points;
+    this.description = description;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public int getPoints() {
+    return this.points;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
 }
