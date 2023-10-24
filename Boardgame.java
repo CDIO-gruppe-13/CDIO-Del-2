187b9290d864ad5088101ec5a837c321a8857930
TheRealChair <62349816+TheRealChair@users.noreply.github.com>
2023-10-24 14:49:49 +0200

    Add array
    add and array, and position to player

--- Boardgame.java
+++ Boardgame.java
@@ -3,6 +3,13 @@ public class Boardgame {
     private Cup cup = new Cup(2);
 
     public void Startgame() {
+        var scanner = new java.util.Scanner(System.in);
+        for (var i = 0; i < this.players.length; i++) {
+            System.out.print("Player " + (i + 1) + " name: ");
+            var playerName = scanner.nextLine();
+            this.players[i].name = playerName;
+        }
 
     }
+
 }
--- Player.java
+++ Player.java
@@ -1,4 +1,5 @@
 public class Player {
     public String name;
+    public int position = -1;
     public int points = 1000;
 }
