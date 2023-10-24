86dccc931c5ebdfe5f494bcff9ee888f76b14bd8
Tobias <tobiasnf@gmail.com>
2023-10-24 14:33:50 +0200

    player class is done

---
+++ Player.java
@@ -0,0 +1,13 @@
+public class Player {
+
+  private String name;
+  private Account account;
+
+  Player(String name, int startBalance) {
+    this.name = name;
+    this.account = new Account(startBalance);
+  }
+  public String toString() {
+    return this.name + ": " + this.account.toString();
+  }
+}
