6bfa3439ea4b4dcf57068f56a9ea061e25586bf0
TheRealChair <62349816+TheRealChair@users.noreply.github.com>
2023-10-25 08:55:57 +0200

    Created a filewriter

--- main.java
+++ main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         var game = new Game(2, 6);
         game.startGame();
+        SaveAndLoad saveAndLoad = new SaveAndLoad();
+        saveAndLoad.Save();
+
     }
 }
